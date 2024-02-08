23e6470d70bf6beb17d135409bfca822165ecc36
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2024-02-08 21:00:12 +0200

    Add files via upload

---
+++ js/localization.js
@@ -0,0 +1,32 @@
+﻿// localization.js
+
+// Функція для встановлення мови
+function setLanguage(language) {
+    // Завантаження файлу локалізації
+    fetch(language + '.json')
+        .then(response => response.json())
+        .then(data => {
+            // Заміна текстів на сторінці
+            Object.keys(data).forEach(key => {
+                const element = document.querySelector(`[data-translate="${key}"]`);
+                if (element) {
+                    element.textContent = data[key];
+                }
+            });
+        })
+        .catch(error => console.error('Localization error:', error));
+}
+
+// Визначення мови за замовчуванням
+let defaultLanguage = 'en';
+
+// Отримання значення параметра ?lang з URL
+const urlParams = new URLSearchParams(window.location.search);
+const langParam = urlParams.get('lang');
+
+// Встановлення мови з параметра ?lang або за замовчуванням
+if (langParam) {
+    setLanguage(langParam);
+} else {
+    setLanguage(defaultLanguage);
+}
---
+++ js/script.js
@@ -0,0 +1,54 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+  const bodyclick = document.querySelector('.body');
+  const headerBurger = document.querySelector('.header__burger');
+  const headerMenu = document.querySelector('.header__menu');
+  const headerOverlay = document.querySelector('.header__overlay');
+
+
+  headerBurger.addEventListener('click', function() {
+    changeClass();
+  });
+
+  headerOverlay.addEventListener('click', function() {
+    changeClass();
+  });
+
+  function changeClass() {
+    bodyclick.classList.toggle('lock');
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    headerOverlay.classList.toggle('lock');
+  }
+
+  resizeHeight()
+  function resizeHeight(){
+      let vh = window.innerHeight;
+      const xxx = document.querySelector('.main');
+      xxx.style = `min-height: ${vh}px`;
+  }
+  window.addEventListener('resize', () => {
+      resizeHeight()
+  });
+
+});
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const tabId = tab.dataset.tab;
+
+     
+        tabContents.forEach(content => {
+            content.classList.add('d-none');
+        });
+
+        
+        tabs.forEach(t => {
+            t.classList.remove('active');
+        });
+
+        document.getElementById(tabId).classList.remove('d-none');
+        tab.classList.add('active');
+    });
+});
